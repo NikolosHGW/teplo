3779225be56fa55ee28b2ae1ce9eaef9564b950e
Ник Николос <nic23172@gmail.com>
2020-01-28 10:45:13 +0300

    TestVersion

---
+++ Core/ButtonTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teplo
+{
+    public class ButtonTest
+    {
+        private bool image;
+        public bool ChangeImage()
+        {
+            return image = !image;
+        }
+    }
+}
--- Teplo/MainWindow.xaml.cs
+++ Teplo/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using Teplo;
 
 namespace Teplo
 {
