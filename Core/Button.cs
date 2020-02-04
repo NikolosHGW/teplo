904ec0e054d72e2945fb43695a3bc9cd0b0f44c8
Ник Николос <nic23172@gmail.com>
2020-02-04 15:16:46 +0300

    CoreButton

---
+++ Core/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Core
+{
+    public class Button : INotifyPropertyChanged
+    {
+        public int Left { get; }
+        public int Top { get; }
+        public Button(int left, int top)
+        {
+            Left = left;
+            Top = top;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        private bool isState;
+        public bool IsState
+        {
+            get
+            {
+                return isState;
+            }
+            set
+            {
+                isState = value;
+                OnPropertyChanged("IsState");
+            }
+        }
+    }
+}
