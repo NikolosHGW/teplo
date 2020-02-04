b41709bd4c883211bc24db92dae49d37ba0f806a
Ник Николос <nic23172@gmail.com>
2020-02-04 10:16:14 +0300

    Revert "NewArchitecture"
    This reverts commit eb4f52c718ea66edb65786f03d461fe68db709f7.

---
+++ UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+
+
+            // если состояние насоса поменялось, то выведи новое состояние (isActive)
+            game.SubscribeOnPumpChanged((x) => Console.WriteLine("pump is now " + x));
+            game.SubscribeOnPumpChanged((x) => Console.WriteLine("pump is now " + x));
+            game.SubscribeOnPumpChanged((x) => Console.WriteLine("pump is now " + x));
+
+            game.TogglePumpState();
+            game.TogglePumpState();
+        }
+    }
+}
