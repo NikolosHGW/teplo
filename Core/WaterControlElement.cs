eb4f52c718ea66edb65786f03d461fe68db709f7
Ник Николос <nic23172@gmail.com>
2020-02-04 10:06:19 +0300

    NewArchitecture

---
+++ Core/WaterControlElement.cs
@@ -0,0 +1,24 @@
+﻿namespace Teplo
+{
+    public delegate void StateDelegate(bool state);
+
+    public abstract class WaterControlElement
+    {
+        public event StateDelegate StateChanged;
+
+        public int Number { get; }
+
+        public bool IsActive { get; private set; }
+
+        protected WaterControlElement(int number)
+        {
+            Number = number;
+        }
+
+        public void ToggleState()
+        {
+            IsActive = ! IsActive;
+            StateChanged?.Invoke(IsActive);
+        }
+    }
+}
